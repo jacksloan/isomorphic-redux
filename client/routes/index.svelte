f3fad24ea6c07255e874c63eb9953aa09e4d89c9
jacksloan <jbsloan10@gmail.com>
2022-04-11 01:20:20 -0500

    better styling - add tailwind and daisyui

--- client/routes/index.svelte
+++ client/routes/index.svelte
@@ -2,41 +2,82 @@
 	import { TodoStoreFactory, type Todo, type TodoStoreFacade } from '$shared';
 	import type { Observable } from 'rxjs';
 	import { onMount } from 'svelte';
+	import { randBook, randCatchPhrase } from '@ngneat/falso';
 
 	let facade: TodoStoreFacade;
 	let description: string;
 	let title: string;
 	let todos: Observable<Todo[]>;
 
+	function newTitle() {
+		title = randBook().title;
+	}
+
+	function newDescription() {
+		description = randCatchPhrase();
+	}
+
+	function createTodo() {
+		facade.createTodo(title, description);
+		newTitle();
+		newDescription();
+	}
+
 	onMount(() => {
+		newDescription();
+		newTitle();
 		facade = TodoStoreFactory.new({ type: 'remote', host: 'http://localhost:3001' }).facade();
 		todos = facade.todos;
 	});
 </script>
 
-<label for="title" />
-<input id="title" type="text" bind:value={title} />
+<div class="px-4 flex flex-row items-center gap-2 h-24">
+	<div class="form-control w-80">
+		<label for="title" class="label">
+			<span class="label-text">Title</span>
+		</label>
+		<input
+			id="title"
+			class="input input-sm input-bordered w-full max-w-xs"
+			type="text"
+			bind:value={title}
+			on:keyup={(e) => (e.key === 'Enter' ? createTodo() : () => {})}
+		/>
+	</div>
 
-<label for="description" />
-<input id="description" type="textarea" bind:value={description} />
+	<div class="form-control w-80">
+		<label for="description" class="label">
+			<span class="label-text">Description</span>
+		</label>
+		<input
+			class="input input-sm input-bordered w-full max-w-xs"
+			id="description"
+			type="textarea"
+			on:keyup={(e) => (e.key === 'Enter' ? createTodo() : () => {})}
+			bind:value={description}
+		/>
+	</div>
 
-<button on:click={() => facade.createTodo(title, description)}>ADD TODO</button>
+	<div class="h-full flex flex-row items-end justify-center content-end" />
+</div>
 
-<table>
-	<thead>
-		<tr>
-			<th>Title</th>
-			<th>Description</th>
-			<th>Complete</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each $todos || [] as todo}
+<div class="p-4 overflow-x-auto">
+	<table class="table w-full table-compact">
+		<thead>
 			<tr>
-				<td>{todo.title}</td>
-				<td>{todo.description}</td>
-				<td>{todo.done}</td>
+				<th>Title</th>
+				<th>Description</th>
+				<th>Complete</th>
 			</tr>
-		{/each}
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			{#each $todos || [] as todo}
+				<tr>
+					<td>{todo.title}</td>
+					<td>{todo.description}</td>
+					<td>{todo.done}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+	content: ['./client/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('daisyui')]
+};
+
+module.exports = config;
